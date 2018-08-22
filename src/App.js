29d44246a1ea1034e5e4877e38b940c0862396eb
Ken Mason <ken.mason52@yahoo.com>
2018-08-21 20:55:37 -0600

    "Added header and 3 sections"

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+/* import logo from './logo.svg'; */
+import logo from './bleach-hero.jpg';
 import './App.css';
 
 class App extends Component {
@@ -7,11 +8,31 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
+          <title>About Me</title>
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
+        <section>
+          <p>
+            I was born. I lived. I took this class.
+          </p>
+        </section>
+        <section>
+          My reasons for attending Helio:
+          <p>
+            <li id="num">Cheap!</li>
+            <li id="num">Get better job than current</li>
+            <li id="num">Class is Fun!</li>
+          </p>
+        </section>
+        <section>
+          <p>
+            <li id="num">Reading</li>
+            <li id="num">Coding</li>
+            <li id="num">Gaming</li>
+          </p>
+        </section>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
         </p>
       </div>
     );
